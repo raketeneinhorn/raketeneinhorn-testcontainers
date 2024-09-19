b6de3137ee4898c4b7280c0a5d091a3f1bd023b6
Raketeneinhorn GmbH <account+github@raketeneinhorn.com>
2024-09-19 21:44:01 +0200

    feat: add BitnamiPostgreSQLContainer

---
+++ src/main/java/com/raketeneinhorn/testcontainers/container/postgresql/BitnamiPostgreSQLContainer.java
@@ -0,0 +1,20 @@
+package com.raketeneinhorn.testcontainers.container.postgresql;
+
+
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+public class BitnamiPostgreSQLContainer<SELF extends BitnamiPostgreSQLContainer<SELF>> extends PostgreSQLContainer<SELF> {
+
+    private static final String IMAGE_NAME = "bitnami/postgresql:16.4.0";
+    private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse(IMAGE_NAME).asCompatibleSubstituteFor(PostgreSQLContainer.IMAGE);
+
+    public BitnamiPostgreSQLContainer() {
+        super(DEFAULT_IMAGE_NAME);
+
+        this.setCommand("/opt/bitnami/scripts/postgresql/run.sh");
+        ((LogMessageWaitStrategy) this.waitStrategy).withTimes(1);
+    }
+
+}
